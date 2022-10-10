de54e076d3217b2aa62d46d554c80eb8951c0965
Donald Geddes <hi@donaldgeddes.ca>
2022-10-09 17:09:26 -0700

    Remove test log

--- src/middleware/log-incoming-request.js
+++ src/middleware/log-incoming-request.js
@@ -3,7 +3,6 @@ import { LISTEN_PORT } from '../constants.js'
 
 /** @type {() => (req: import('express').Request, _: import('express').Response, done: import('express').NextFunction) => void} */
 export const logIncomingRequest = () => (req, _, done) => {
-  console.log('got here')
   const method = color.bold(req.method)
   const url = `${req.protocol}://${req.hostname}:${LISTEN_PORT}${req.url}`
   console.log(`${method} ${url}`)
