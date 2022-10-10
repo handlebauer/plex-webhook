26eeb8e9905340928b90f8909bb2f73adac197d3
Donald Geddes <hi@donaldgeddes.ca>
2022-10-09 22:45:40 -0700

    Parse mult-part form data

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 import express from 'express'
+import multer from 'multer'
 
-import { logIncomingRequest } from './middleware/log-incoming-request.js'
+// import { logIncomingRequest } from './middleware/log-incoming-request.js'
 
 const server = express()
 
-server.use(express.json())
-server.use(logIncomingRequest())
+// server.use(express.json())
+// server.use(logIncomingRequest())
 
-server.use('*', (_, res) => {
+server.use('*', multer().none(), (req, res) => {
+  console.log(req.body)
   res.json({ success: true })
 })
 
