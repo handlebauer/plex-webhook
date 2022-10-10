674dc99893d5f4d1c56d00af9dcf8e9193a3f103
Donald Geddes <hi@donaldgeddes.ca>
2022-10-09 22:58:34 -0700

    Parse mult-part form data rawly

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 import express from 'express'
-import multer from 'multer'
-
+import bodyParser from 'body-parser'
 // import { logIncomingRequest } from './middleware/log-incoming-request.js'
 
 const server = express()
 
+server.use(
+  bodyParser.raw({ inflate: true, limit: '100kb', type: 'multipart/form-data' })
+)
 // server.use(express.json())
 // server.use(logIncomingRequest())
 
-server.use('*', multer().none(), (req, res) => {
-  console.log(req.body)
+server.use('*', (req, res) => {
+  console.log(new TextDecoder('utf-8').decode(req.body))
   res.json({ success: true })
 })
 
